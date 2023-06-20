2714fed2b73c90e227ab4e4ab99fdd486cd91740
Sedem <boafoj403@gmail.com>
2023-06-19 20:38:43 -0700

    New

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints number 0-9
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
