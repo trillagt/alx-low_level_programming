c468d83628fd35db0aa52299e9a8f295aaeeae41
Sedem <boafoj403@gmail.com>
2023-07-06 20:52:22 -0700

    New commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int actual_prime(int n, int i);
+
+/**
+ * is_prime_number - says if an integer
+ * @n: rettr
+ *
+ * Return: 1
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - calculates if a number
+ * @n: qwqs
+ * @i:
+ *
+ * Return: 1
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
+}
