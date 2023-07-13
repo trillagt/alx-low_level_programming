1d777def489f664390357a55cf491e684a024ec4
Sedem <boafoj403@gmail.com>
2023-07-12 21:15:02 -0700

    New commit

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - a function that copies memory area
+ * @dest: memory where it is stored
+ * @src: memory where it is copied
+ * @n: number of bytes
+ *
+ * Return: copied memory with n bytes changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int r = 0;
+    unsigned int i = n;
+
+    for (; r < i; r++)
+    {
+        dest[r] = src[r];
+        n--;
+    }
+
+    return dest;
+}
